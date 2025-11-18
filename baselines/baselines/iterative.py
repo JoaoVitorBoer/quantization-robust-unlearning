a56ed9260362d7bb93d44a09743fae6fb121efb2
Joao Vitor Boer Abitante <joaoavitor.abitante@gmail.com>
2025-11-18 14:46:13 -0300

    First files commit

---
+++ baselines/baselines/iterative.py
@@ -0,0 +1,195 @@
+from .utils import load_model_and_tokenizer, load_model
+from .dataset import ForgetRetainDataset
+
+import torch
+import torch.nn.functional as F
+from torch.cuda import device_count
+import transformers
+from transformers import Trainer, AutoModelForCausalLM
+import numpy as np
+
+# model_dir is the address of learned target model
+# data_file is the address of the forget set
+# out_dir is the address of the unlearned model
+# retain_data_file is the address of the retain set
+def unlearn(
+    model_dir: str,
+    data_file: str,
+    out_dir: str,
+    retain_data_file: str | None = None,
+    loss_type: str = 'ga',
+    per_device_batch_size: int = 2,
+    epochs: int = 5,
+    learning_rate=1e-5,
+    max_len: int = 4096,
+    tokenizer_dir: str | None = None,
+    resume_from_checkpoint: bool = False,
+    alpha: float = 1.0,
+):
+    if 'gd' in loss_type:
+        assert retain_data_file is not None, "Retain data must be specified for grad_diff."
+    # load target learned model, here the model is finetuned on Llama2, so we load its tokenizer
+    model, tokenizer = load_model_and_tokenizer(
+        model_dir,
+        tokenizer_dir=tokenizer_dir
+    )
+
+    # load reference model (learned model) for negative preference optimization and KL divergence constraints
+    ref_model = (
+        load_model(model_dir)
+        if 'npo' in loss_type or 'kl' in loss_type or 'rmu' in loss_type
+        else None
+    )
+    dataset = ForgetRetainDataset(
+        data_file,
+        tokenizer=tokenizer,
+        retain_file_path=retain_data_file,
+        max_len=max_len
+    )
+
+    if device_count() == 0:
+        raise ValueError("Device not detected!")
+
+    training_args = transformers.TrainingArguments(
+        output_dir=out_dir,
+        per_device_train_batch_size=per_device_batch_size,
+        learning_rate=learning_rate,
+        save_strategy='epoch',  # Save every epoch
+        num_train_epochs=epochs,
+        optim='adamw_torch',
+        lr_scheduler_type='constant',
+        bf16=True,
+        report_to='none'        # Disable wandb
+    )
+    if 'qat' in loss_type:
+        pass
+    else:
+        trainer = IterativeUnlearner(
+            model=model,
+            ref_model=ref_model,
+            tokenizer=tokenizer,
+            train_dataset=dataset,
+            args=training_args,
+            data_collator=dataset.get_collate_fn(),
+            loss_type=loss_type,
+            alpha=alpha
+        )
+    model.config.use_cache = False  # silence the warnings.
+    trainer.train(resume_from_checkpoint=resume_from_checkpoint)
+    trainer.save_model(out_dir)
+
+
+#### iterative unlearner from the benchmark MUSE ####
+class IterativeUnlearner(Trainer):
+    """Source: https://github.com/locuslab/tofu/blob/main/dataloader.py
+    """
+
+    def __init__(self, *args,
+                 loss_type: str = 'ga',
+                 ref_model: AutoModelForCausalLM | None = None,
+                 beta: float = 0.1,
+                 alpha: float = 1.0,
+                 threshold: int = 90,
+                 **kwargs):
+        self.loss_type = loss_type
+        self.ref_model = ref_model
+        self.beta = beta    # Only relevant when `'po' in self.loss_type`
+        self.alpha = alpha  # Weighting for retain data loss
+        if ref_model is not None:
+            # assert 'po' in self.loss_type or 'kl' in self.loss_type
+            ref_model = ref_model.eval()
+
+        super().__init__(*args, **kwargs)
+
+    # the input is get_collate_fn() from ForgetRetainDataset
+    def compute_loss(self, model, x, return_outputs=False):
+        """Source: https://github.com/licong-lin/negative-preference-optimization/blob/main/synthetic/mymodel.py
+        """
+        
+        ### 1. Run model ###
+        x_f, x_r = x
+        outputs_f = model(
+            x_f['input_ids'],
+            labels=x_f['labels'] if 'labels' in x_f else x_f['input_ids'].clone(),
+            attention_mask=x_f['attention_mask'] if 'attention_mask' in x_f else torch.ones_like(x_f['input_ids'], dtype=torch.bool)
+        )
+        loss_f = outputs_f.loss
+
+        if 'gdr' in self.loss_type or 'klr' in self.loss_type or 'rmu' in self.loss_type:
+            outputs_r = model(
+                x_r['input_ids'],
+                labels=x_r['labels'] if 'labels' in x_r else x_r['input_ids'].clone(),
+                attention_mask=x_r['attention_mask'] if 'attention_mask' in x_r else torch.ones_like(x_r['input_ids'], dtype=torch.bool)
+            )
+            loss_r = outputs_r.loss
+
+        if 'klf' in self.loss_type or 'npo' in self.loss_type or 'rmu' in self.loss_type:
+            with torch.no_grad():
+                outputs_f_ref = self.ref_model(
+                    x_f['input_ids'],
+                    labels=x_f['labels'] if 'labels' in x_f else x_f['input_ids'].clone(),
+                    attention_mask=x_f['attention_mask'] if 'attention_mask' in x_f else torch.ones_like(x_f['input_ids'], dtype=torch.bool)
+                )
+
+        if 'klr' in self.loss_type:
+            with torch.no_grad():
+                outputs_r_ref = self.ref_model(
+                    x_r['input_ids'],
+                    labels=x_r['labels'] if 'labels' in x_r else x_r['input_ids'].clone(),
+                    attention_mask=x_r['attention_mask'] if 'attention_mask' in x_r else torch.ones_like(x_r['input_ids'], dtype=torch.bool)
+                )
+
+        ### 2. Compute Loss ###
+        loss = 0
+
+        if 'ga' in self.loss_type:
+            loss += -loss_f
+
+        elif 'npo' in self.loss_type:
+            neg_log_ratio = outputs_f_ref.logits - outputs_f.logits
+            loss += -F.logsigmoid(self.beta * neg_log_ratio).mean() * 2 / self.beta
+
+        # else:
+        #     raise NotImplementedError("Cannot infer the given loss type.")
+
+        if 'gdr' in self.loss_type:
+            loss += loss_r * self.alpha
+
+        if 'klf' in self.loss_type:
+            raise NotImplementedError("KL forget not implemented yet!")
+
+        if 'klr' in self.loss_type:
+            kl_r = F.kl_div(
+                outputs_r.logits,
+                outputs_r_ref.logits,
+                reduction = 'batchmean',
+                log_target = True
+            )
+            loss += kl_r * self.alpha
+
+        if 'rmu' in self.loss_type:
+            # Sample a unit vector `u`
+            u = torch.rand(model.config.hidden_size).uniform_(-1, 1).to(outputs_f.logits.device)
+            u /= u.norm()
+
+            # Compute activation differences
+            activations_f = model(x_f['input_ids'], output_hidden_states=True).hidden_states[-1]
+            activations_r = model(x_r['input_ids'], output_hidden_states=True).hidden_states[-1]
+            activations_r_ref = self.ref_model(x_r['input_ids'], output_hidden_states=True).hidden_states[-1]
+
+            L_forget = torch.mean(torch.norm(activations_f - self.alpha * u, dim=-1) ** 2)
+            L_retain = torch.mean(torch.norm(activations_r - activations_r_ref, dim=-1) ** 2)
+
+            loss += L_forget + self.alpha * L_retain
+
+        return (loss, outputs_f) if return_outputs else loss
+
+
+    def prediction_step(self, model, x, prediction_loss_only: bool, ignore_keys=None):
+        input_ids, labels, attention_mask = x
+        # forward pass
+        with torch.no_grad():
+            outputs = model(input_ids, labels=labels, attention_mask=attention_mask)
+            logits = outputs.logits
+            loss = outputs.loss
+        return (loss, logits, labels)
